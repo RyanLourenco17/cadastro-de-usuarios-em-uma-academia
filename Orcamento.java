ee8b84e5258af903646233e2890ca815ee60d244
Ryan Campos <ryanlgithub@gmail.com>
2024-05-15 17:55:10 -0300

    Modificando a classe Orçamento

--- Orcamento.java
+++ Orcamento.java
@@ -1,64 +1,81 @@
 import java.util.Scanner;
 
 public class Orcamento {
-    private double precoPlanoAnual = 80.00;
-    private double precoPlanoMensal = 100.00;
-    private double percentualDanca = 0.25;
-    private double percentualPersonal = 0.55;
-    private double percentualDefesaP = 0.40;
-    private double precoFinal;
+    //preço dos produtos principais
+    double precoPlanoAnual = 80.00;
+    double precoPlanoMensal = 100.00;
+    //Percentuais de alteração dos subprodutos
+    double percentualDanca = 40;
+    double percentualPersonal = 65;
+    double percentualDefesaP = 50;
+    double semAlteracao = 0;
+    double precoInicial = 0.00;
+    double precoFinal;
+
 
     public void calcularOrcamento() {
         Scanner scanner = new Scanner(System.in);
+        // instanciar
+        Orcamento Valores = new Orcamento();
+        // Pronto. Só tinha que instanciar e chamar no local preferível.
+        Main main = new Main();
 
-        System.out.println("Escolha o produto:");
+        // Exibir opções de produtos principais
+        System.out.println("Escolha do Plano:");
         System.out.println("1. Plano Anual / R$80,00");
         System.out.println("2. Plano Mensal / R$100,00");
-        System.out.print("Opção: ");
+        System.out.println("Opção: ");
+
+        //case switch
         int opcao = scanner.nextInt();
         double precoInicial = 0.00;
-        switch (opcao) {
+        switch (opcao){
             case 1:
-                precoInicial = precoPlanoAnual;
+                precoInicial = Valores.precoPlanoAnual;
                 break;
             case 2:
-                precoInicial = precoPlanoMensal;
+                precoInicial = Valores.precoPlanoMensal;
                 break;
             default:
-                System.out.println("Opção errada");
+                System.out.println("Opçao errada");
                 return;
         }
+        main.clearScreen();
+        System.out.println("Preço do plano escolhido R$"+precoInicial);
 
-        System.out.println(precoInicial);
-
-        System.out.println("1. Dança / 25% do valor inicial");
-        System.out.println("2. Personal / 55% do valor inicial");
-        System.out.println("3. Defesa Pessoal / 40% do valor inicial");
+        
+        //exibir subprodutos
+        System.out.println("Opções de aulas e treinos."+ 
+        "-> Dança custa R$40,00 "+
+        "-> Personal custa R$65,00 "+
+        "-> Defesa Pessoal custa R$50,00");
+        System.out.println("1. Dança");
+        System.out.println("2. Personal");
+        System.out.println("3. Defesa Pessoal");
         System.out.println("4. Nao escolher nenhum");
         System.out.print("Opção: ");
         int opcao2 = scanner.nextInt();
-        switch (opcao2) {
+        switch (opcao2){
             case 1:
-                precoInicial += (precoInicial *= percentualDanca);
+                precoInicial += Valores.percentualDanca ;
                 break;
             case 2:
-                precoInicial += (precoInicial *= percentualPersonal);
+                precoInicial += Valores.percentualPersonal;
                 break;
             case 3:
-                precoInicial += (precoInicial *= percentualDefesaP);
+                precoInicial += Valores.percentualDefesaP;
+                break;
             case 4:
-                precoInicial += 0;
+                precoInicial += Valores.semAlteracao;
+                break;
             default:
                 System.out.println(precoInicial);
                 return;
         }
-
-        // Atribuir o preço final calculado
-        precoFinal = precoInicial;
         
-        System.out.println("R$" + precoInicial);
-
-    }
+        System.out.println("O valor da sua mesalidade ficou por R$" + precoInicial);
+    } 
+    
     @Override
     public String toString() {
         // Retornar uma representação legível do orçamento
